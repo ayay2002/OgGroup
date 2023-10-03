8554a69eeb2a7865515c48dba12b8c271f087bdd
Cpanelli <133393733+Cpanelli@users.noreply.github.com>
2023-10-02 20:30:55 -0400

    fixed the picture and put our names on right side of page

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-var list = document.getElementById("main")
-
-function getApi() {
-  var requestUrl = 'https://pokeapi.co/api/v2/pokemon';
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      console.log(data.results);
-      for (var i = 0; i < data.results.length; i++) {
-        console.log(data.results[i].name)
-        var name = document.createElement("div")
-        name.textContent = data.results[i].name;
-        list.appendChild(name)
-      }
-    });
+let promises = [];
+for(let i = 0; i < 151; i++) {
+  promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`)
+  .then(response => response.json()));
 }
-getApi()
+
+promises.all(promises)
+.then(results => results.forEach(pokemonObj => {
+  let pokemon = {
+    name: pokemonObj.name,
+    id: pokemonObj.id,
+    image: pokemonObj.sprites.front_default,
+    caught: false,
+    nickname: ""}}
+))
+  card.querySelector(".caught-btn").addEventListner("click", () => {
+    pokemon.caught = true;
+  updatePokemon(pokemon, {
+    "caught": pokemon.caught
+  })
+    .then((pokemonData) => {
+      card.querySelector(".caught-btn").disabled = true;
+      card.querySelector(".caught-btn").innerText = "Caught!"
+card.style["background-color"] = "#caedcc";
+    });
+    
+    
+
+  });
+  FormData.addEventListner("submit", (e) => {
+    e.preventDefault();
+
+updatePokemon(pokemon, {
+  "nickname": e.target.nickname.value})
+  .then((pokemonData)=> {
+    setNickname(pokemonData, pokeNickname, form, removeHyperlink, addHyperlink);
+  })
+})
